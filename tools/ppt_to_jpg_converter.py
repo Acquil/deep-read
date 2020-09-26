9e10634b3305b3bb1d727f92a982710621cac11f
Sree Deepack R <sreedeepackr@gmail.com>
2020-09-26 16:15:12 +0530

    Update - Created a function to convert ppt to jpg
    Former-commit-id: 244567e2de055d1d516f41a56030c9c044c7bd07

--- tools/ppt_to_jpg_converter.py
+++ tools/ppt_to_jpg_converter.py
@@ -11,6 +11,11 @@ import win32com.client
 # base path - Powerpoint files in this path will be converted to jpg
 base_path = 'A:\\sem_9\\open_source\\ppts\\'
 
+# render path - Rendered Images of powerpoint files will be saved in this path
+render_path = 'A:\\sem_9\\open_source\\images_rendered_from_powerpoint\\'
+
+image_start_index = 0
+
 
 
 # Initialize PPT
@@ -19,13 +24,25 @@ def init_powerpoint():
     powerpoint.Visible = 1
     return powerpoint
 
+# PPT to JPG converter
+def ppt_to_jpg(powerpoint, inputFileName, formatType = 32):
+    global image_start_index
+    deck = powerpoint.Presentations.Open(inputFileName)
+    deck.SaveAs(render_path + "temp\\" + '.jpg', 17)
+    deck.Close()
+
 
 # Scan for PPT files in the folder
 def convert_files_in_folder(powerpoint, folder):
     files = os.listdir(folder)
     pptfiles = [f for f in files if f.endswith((".ppt", ".pptx"))]
     print("Powerpoint files Scanned", pptfiles)
-  
+    for pptfile in pptfiles:
+        fullpath = os.path.join(base_path, pptfile)
+        ppt_to_jpg(powerpoint, fullpath)       
+
+
+
 
 if __name__ == "__main__":
     powerpoint = init_powerpoint()
