73942a5737b2707798525ffe86be60ef22214ef6
Sree Deepack R <sreedeepackr@gmail.com>
2020-09-26 16:13:42 +0530

    Update - Created a function which can search for all ppts in a folder

--- tools/ppt_to_jpg_converter.py
+++ tools/ppt_to_jpg_converter.py
@@ -8,13 +8,26 @@ import win32com
 import win32com.client
 
 
+# base path - Powerpoint files in this path will be converted to jpg
+base_path = 'A:\\sem_9\\open_source\\ppts\\'
+
+
+
 # Initialize PPT
 def init_powerpoint():
     powerpoint = win32com.client.Dispatch('PowerPoint.Application')
     powerpoint.Visible = 1
-    return powerpoint  
+    return powerpoint
 
+
+# Scan for PPT files in the folder
+def convert_files_in_folder(powerpoint, folder):
+    files = os.listdir(folder)
+    pptfiles = [f for f in files if f.endswith((".ppt", ".pptx"))]
+    print("Powerpoint files Scanned", pptfiles)
+  
 
 if __name__ == "__main__":
     powerpoint = init_powerpoint()
+    convert_files_in_folder(powerpoint, base_path)
     powerpoint.Quit()
