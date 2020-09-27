4243ba7c95264870c6e2cb94b896fbb9a72e4953
ashwin <nashwin2804@gmail.com>
2020-09-27 19:16:42 +0530

    Created tool for extracting images from video

--- tools/ppt_to_jpg_converter/driver_ppt_to_jpg.py
+++
@@ -1,7 +0,0 @@
-from ppt_to_jpg_convertor import *
-
-if __name__ == '__main__':
-    base_path = 'C:\\open_source\\ppts\\'
-    render_path = 'C:\\open_source\\images_rendered_from_powerpoint\\'
-    ppt_to_jpg_converter = PPT_to_JPG_Convertor(base_path,render_path)
-    ppt_to_jpg_converter.convert_ppt_to_jpg()
