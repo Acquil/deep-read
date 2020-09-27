3eff18e1d8fbbdd71a4f8a6ece7a1fa587de4582
Acquil <acquil98@gmail.com>
2020-09-27 15:31:16 +0530

    Refactored requirements and added newline to EOFs
    Former-commit-id: 388a7aa765106776746247693177814a011b1ea5

--- src/app/speech_text/_deepspeech/requirements.sh
+++ src/app/speech_text/_deepspeech/requirements.sh
@@ -1,8 +1,5 @@
-pip install deepspeech-gpu==0.6.0
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
 curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.6.0/deepspeech-0.6.0-models.tar.gz
 tar -xvzf deepspeech-0.6.0-models.tar.gz
-pip install pydub
-pip install deepsegment
-pip install tensorflow==2.2.0
-pip install keras==2.3.1
-pip install numpy
