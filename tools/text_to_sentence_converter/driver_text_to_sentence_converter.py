5672fd635c66224b7b6d5e47d39f70bc0e62c417
ashwin <nashwin2804@gmail.com>
2020-09-28 20:12:17 +0530

    Created module for extracting sentences from text
    Former-commit-id: 3282d6ea28567ae3ab19483b3dfcfb27a008e2e3

---
+++ tools/text_to_sentence_converter/driver_text_to_sentence_converter.py
@@ -0,0 +1,21 @@
+from text_to_sentence_converter import Text_to_Sentence_Converter
+
+text = """ Read()
+
+* The syntax of the read system call is
+
+* number=read(fd, buffer, count}
+* fds the file descriptor returned by open
+
+* buffer is the address of a data structure in the user process that will contain
+the read data on successful completion of the call
+
+* count is the number of bytes the user wants to read
+* number is the number of bytes actually read"""
+
+text_to_sentence_converter = Text_to_Sentence_Converter(text)
+sentences = text_to_sentence_converter.convert()
+
+for i in range(0,len(sentences)):
+    print("Sentence",i)
+    print("    "," ".join(sentences[i].split()))
---
+++ tools/text_to_sentence_converter/text_to_sentence_converter.py
@@ -0,0 +1,35 @@
+import nltk
+nltk.download('punkt')
+
+from nltk import sent_tokenize
+from nltk.tokenize import word_tokenize
+
+class Text_to_Sentence_Converter:
+
+    def __init__(self, text):
+        self.text = text
+
+    def convert(self):
+        nltk_extracted_sentences = sent_tokenize(self.text)
+
+        sentences = []
+        for sentence in nltk_extracted_sentences:
+            lines = sentence.split("\n")
+            is_new_sentence = True
+            for line in lines:
+                tokens = word_tokenize(line)
+                if is_new_sentence:
+                    if len(tokens) <= 3:
+                        continue
+                    else:
+                        sentences.append(line)
+                        is_new_sentence = False
+                else:
+                    if len(tokens)==0:
+                        is_new_sentence = True
+                    else:
+                        if tokens[0] == '*':
+                            sentences.append(line)
+                        else:
+                            sentences[len(sentences)-1] += " " + line
+        return sentences
