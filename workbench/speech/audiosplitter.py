7dc08b0a1da41a0f5879852a7a66cd89afb653ac
Acquil <acquil98@gmail.com>
2020-09-29 17:00:17 +0530

    Added audiosplitter class and split Recognizer into Recognizer and RecognizerSegments
    audiosplitter is for splitting audio files into temporary chunks
    
    RecognizerSegments is the Recognizer class from the previous commit but now recommended only for audio files < 1 min. The new Recognizer class now uses multiprocessing.

---
+++ workbench/speech/audiosplitter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from pydub import AudioSegment
+import math
+
+class SplitWavAudio():
+    def __init__(self, folder, filename):
+        self.folder = folder
+        self.filename = filename
+        self.filepath = folder + '/' + filename
+        
+        self.audio = AudioSegment.from_wav(self.filepath)
+    
+    def get_duration(self):
+        return self.audio.duration_seconds
+    
+    def single_split(self, from_min, to_min, split_filename):
+        t1 = from_min * 60 * 1000
+        t2 = to_min * 60 * 1000
+        split_audio = self.audio[t1:t2]
+        split_audio.export(self.folder + '/' + split_filename, format="wav")
+        
+    def multiple_split(self, min_per_split):
+        total_mins = math.ceil(self.get_duration() / 60)
+        number_splits = None
+        
+        for i in range(0, total_mins, min_per_split):
+            split_fn = str(i) + '_' + self.filename
+            self.single_split(i, i+min_per_split, split_fn)
+            print(str(i) + ' Done')
+            if i == total_mins - min_per_split:
+                print('All splitted successfully')
+                number_splits = i
+        
+        return number_splits
