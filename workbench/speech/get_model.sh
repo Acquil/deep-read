e67944c7a1ab9568f600fdfd2704cb6e61d6796d
Acquil <acquil98@gmail.com>
2020-09-29 09:46:37 +0530

    Changed vosk model
    Former-commit-id: 7d6e16e2e385851f4e1e5a611c649515bafdc2a0

--- workbench/speech/get_model.sh
+++ workbench/speech/get_model.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://alphacephei.com/kaldi/models/vosk-model-en-us-aspire-0.2.zip
-unzip vosk-model-en-us-aspire-0.2.zip
-mv vosk-model-en-us-aspire-0.2 model
+# wget https://alphacephei.com/kaldi/models/vosk-model-en-us-aspire-0.2.zip
+# unzip vosk-model-en-us-aspire-0.2.zip
+# mv vosk-model-en-us-aspire-0.2 model
+wget http://alphacephei.com/vosk/models/vosk-model-small-en-us-0.3.zip
+unzip vosk-model-small-en-us-0.3.zip
+mv vosk-model-small-en-us-0.3 model
