218ca79cc0fe3b08dfded48990583c3f3190240a
Sree Deepack R <sreedeepackr@gmail.com>
2020-10-02 21:39:37 +0530

    Update Added Cards after drive link is uploaded, changed theme and nav bar position
    Former-commit-id: 79e99708576ab0c96ca6d4ee31f6d4b030078a71

---
+++ src/web/deep-read-ui/src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className='reports'>
+      <h1>About Us Page</h1>
+    </div>
+  );
+}
+
+export default AboutUs;
--- src/web/deep-read-ui/src/pages/Home.js
+++ src/web/deep-read-ui/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+function Home() {
   return (
-    <div className="page">
+    <div className='products'>
       <h1>Home Page</h1>
     </div>
   );
-};
+}
+
 export default Home;
