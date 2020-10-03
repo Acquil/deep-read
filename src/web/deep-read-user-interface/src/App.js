a4038f6790c41c74bedef6c782c62f8a2fc79189
Sree Deepack R <sreedeepackr@gmail.com>
2020-10-03 20:55:31 +0530

    Update Added Nav bar in app file

--- src/web/deep-read-user-interface/src/App.js
+++ src/web/deep-read-user-interface/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './components/NavigationBar'
 
-
 export default function App() {
   return (
     <NavigationBar />
