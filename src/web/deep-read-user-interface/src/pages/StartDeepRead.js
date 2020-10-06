a22dcbcb9cc1170dec6a4e065c93b15e8bd6fb9b
Sree Deepack R <sreedeepackr@gmail.com>
2020-10-06 08:51:43 +0530

    Update Design changes for start deep read
    Former-commit-id: fb7be5ad11a4094681c667dcd22df35f1c168886

--- src/web/deep-read-user-interface/src/pages/StartDeepRead.js
+++ src/web/deep-read-user-interface/src/pages/StartDeepRead.js
@@ -1,14 +1,6 @@
-import { makeStyles, Paper, Grid } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import { makeStyles, Paper, Grid, Button, TextField } from '@material-ui/core';
 import React from 'react';
-import FormControl from '@material-ui/core/FormControl';
-import clsx from 'clsx';
 import ThreeSixtyIcon from '@material-ui/icons/ThreeSixty';
-
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,9 +18,10 @@ const useStyles = makeStyles((theme) => ({
     width: '25ch',
   },
   paper: {
+    background: '#eeeeee',
+    margin: `${theme.spacing(1)}px auto`,
     padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+
   },
   topSpacing10: {
     marginTop: '10px'
@@ -40,12 +33,6 @@ const useStyles = makeStyles((theme) => ({
   },
   fullWidthElement: {
     width: '100%'
-  },
-  margin: {
-    margin: theme.spacing(1),
-  },
-  textField: {
-    width: '25ch',
   },
   labelWidth:{
     fontSize:"70px"
@@ -100,45 +87,11 @@ function StartDeepRead() {
     <div>
       <div><h1><strong>Start deep-read</strong></h1></div>
       <div className={classes.topSpacing30}>
-        {/* <div><TextField className={classes.fullWidthElement} id="outlined-basic" label="Google Drive Link" variant="outlined" onChange={updateGDriveTextBox} /></div> */}
-        {/* <OutlinedInput
-            id="outlined-adornment-password"
-            onChange={updateGDriveTextBox}
-            label="Google Drive Link"
-            endAdornment={
-              <InputAdornment position="end">
-                <BackupSharpIcon
-                  aria-label="toggle password visibility"
-                  onClick={sendGDriveLinkAPI}
-                  edge="end"
-                >
-                </BackupSharpIcon>
-              </InputAdornment>
-            }
-          /> */}
-        {/* <div className={classes.topSpacing10}><Button className={classes.fullWidthElement} variant="outlined" label="Process" onClick={sendGDriveLinkAPI}>Process</Button></div> */}
-        <div>
-          <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password"><div>Google Drive Link</div></InputLabel>
-            <OutlinedInput
-              className={classes.fullWidthElement}
-              id="outlined-adornment-password"
-              onChange={updateGDriveTextBox}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={sendGDriveLinkAPI}
-                    onMouseDown={sendGDriveLinkAPI}
-                    edge="end"
-                  >
-                  <ThreeSixtyIcon></ThreeSixtyIcon>
-                  </IconButton>
-                </InputAdornment>
-              }
-              labelWidth={130}
-            />
-          </FormControl>
-        </div>
+        <Paper className={classes.paper}>
+        <div><TextField className={classes.fullWidthElement} id="outlined-basic" label="Google Drive Link" variant="outlined" onChange={updateGDriveTextBox} /></div>
+        <div className={classes.topSpacing10}><Button className={classes.fullWidthElement} startIcon={<ThreeSixtyIcon />} variant="outlined"  label="Process" onClick={sendGDriveLinkAPI}>Process</Button></div>
+        </Paper>
+        
       </div>
       <div className={classes.topSpacing30}>
         {displayDataForGDriveLink()}
