c888b3aa4b1788832a419e0bb408b020c10284a1
Acquil <acquil98@gmail.com>
2020-10-07 13:40:32 +0530

    Cleaned up workbench files and notebooks

--- workbench/speech/sample_audio.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# wget https://webgate.ec.europa.eu/sr-files/vod/9d64e8c6bacfd2a98415686a6af75cfd/node/sr-speech_29159_482.mp4
-# mv sr-speech_29159_482.mp4 input.mp4
-# ffmpeg -i input.mp4 -vn input.wav
-# ffmpeg -i input.wav -acodec pcm_s16le -ac 1 -ar 16000 output.wav
-# echo "Removing input.mp4"
-# rm input.mp4
