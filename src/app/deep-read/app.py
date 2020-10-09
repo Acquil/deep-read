d7603b9acf95635bb206d109d964d7e215f9eb5a
Aquil Hassan <acquil98@gmail.com>
2020-10-09 13:04:17 +0000

    Update: CORS origin

--- src/app/deep-read/app.py
+++ src/app/deep-read/app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from flask import Flask
+from flask_cors import CORS
 from apis import api
 # https://flask-restx.readthedocs.io/en/latest/quickstart.html
 
 app = Flask(__name__)
+# CORS
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 api.init_app(app)
 
 app.run(debug=True)
