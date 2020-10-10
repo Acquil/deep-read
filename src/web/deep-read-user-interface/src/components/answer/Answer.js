557fbf90f4fc3d2cea70c67497077234eabb6369
Sree Deepack R <sreedeepackr@gmail.com>
2020-10-11 00:21:02 +0530

    Update Added MCQs

---
+++ src/web/deep-read-user-interface/src/components/answer/Answer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Answer.css';
+
+const Answer = (props) => {
+    let answers = Object.keys(props.answer)
+        .map((qAnswer, i) => (
+            <li
+            className=
+            {
+                props.correctAnswer === qAnswer ?
+                'correct' : 
+                props.clickedAnswer === qAnswer ? 
+                'incorrect' : ''
+            }
+            onClick={() => props.checkAnswer(qAnswer)}
+            key={qAnswer}>
+                {props.answer[qAnswer]}
+            </li>
+        ));
+
+        return (
+            <>
+                <ul disabled={props.clickedAnswer ? true : false} className="Answers">
+                    {answers}
+                </ul>
+                <div>
+                    {
+                        props.correctAnswer ?
+                        'Correct Answer!' : 
+                        props.clickedAnswer ? 'Incorrect Answer!' : ''
+                    }
+                </div>
+            </>
+        );
+}
+
+export default Answer;
