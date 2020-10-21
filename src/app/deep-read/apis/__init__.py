328038817a93a7c917295653526dad9d9f833df9
ashwin <nashwin2804@gmail.com>
2020-10-21 09:16:51 +0530

    Add APIs for summarization
    Currently summarizing only video text
    // TODO: Summarize audio text

--- src/app/deep-read/apis/__init__.py
+++ src/app/deep-read/apis/__init__.py
@@ -3,6 +3,7 @@ from .gdrive_api import api as ns1
 from .speech_api import api as ns2
 from .mcq_generator_api import api as ns3
 from .tester_api import api as ns4
+from .summarizer_api import api as ns5
 
 api = Api(
     version="0.1",
@@ -14,4 +15,5 @@ api.add_namespace(ns1)
 api.add_namespace(ns2)
 api.add_namespace(ns3)
 api.add_namespace(ns4)
+api.add_namespace(ns5)
 
---
+++ src/app/deep-read/apis/summarizer_api.py
@@ -0,0 +1,99 @@
+from flask import Flask, jsonify, Response
+from flask_restx import Resource, Namespace, fields
+from core.video_to_text_converter import Video_to_Text_Converter
+from core.summarizer import *
+from math import ceil
+import pymongo
+import os
+import json
+import soundfile
+
+from threading import Thread
+import threading
+
+from db import DRVideoNotFound, DRVideo
+from db.factory import create_repository
+from settings import REPOSITORY_NAME, REPOSITORY_SETTINGS, TRANSCRIPTION_WORKERS
+
+# DB
+repository = create_repository(REPOSITORY_NAME, REPOSITORY_SETTINGS)
+# Namespace
+api = Namespace('summarizer', description='Summarization related operations')
+
+# TODO Response Marshalling
+
+@api.route('/post/<path:file>')
+@api.param('file','File to be summarized')
+class SummarizerRequest(Resource):
+    
+
+    def post(self, file):        
+        id = file
+        # check if file exists else
+        file = f"core/temp/{file}.mp4"
+        if not os.path.isfile(file):
+            api.abort(404, "file does not exist", custom="file should be uploaded first")
+
+
+        #TODO check if doc already exists
+        output = {'summary': "",  'status':'In Process'}
+        repository.update(dr_key = id, field= 'summary', data=output)
+
+        Thread(target = self.summarize_task, args=(file, id)).start()
+        return {
+            'id': id,
+            'status': 'In Process'
+        }
+
+    # Thread to spawn more processes and summarize in background
+    def summarize_task(self, file, id):
+        #Extract Text from Video
+        video_path = file#f"core/temp/{file}.mp4"
+        video_to_text_converter = Video_to_Text_Converter(video_path = video_path) 
+        video_text = video_to_text_converter.convert()
+        print('Extracted Video Text')
+        
+        #summarize the video and audio text
+        # TODO summarize audio text
+        audio_text = ""
+        text = video_text + audio_text
+        filtered_text = filter_text(text.split(". "))
+        chunks = list(get_chunks(filtered_text, 16))
+        final_text = list()
+        threads = list()
+
+        for chunk in chunks:
+            t = threading.Thread(target=spell_checker, args=(chunk,final_text))
+            threads.append(t)
+            t.start()
+            
+        _ = [t.join() for t in threads]
+
+        summarizer = Summarizer(final_text)
+        summary = summarizer.get_summary()
+
+        
+        repository.update(dr_key = id, field= 'image_text', data=video_text)
+        output = {'summary': summary, 'status':'Success'}
+        repository.update(dr_key = id, field= 'summary', data= output)
+        print('Summarized')
+
+
+@api.route('/get/<file_id>')
+@api.param('file_id','File ID')
+class SummarizeResponse(Resource):
+    """
+    Poll for summary
+    """
+    def get(self, file_id):
+        data = repository.get_one(file_id)
+        return ({
+            "summary": data.summary['summary'],
+            "status": data.summary['status']
+        })
+
+
+@api.errorhandler(DRVideoNotFound)
+def video_no_found(error):
+    """no such video exists"""
+    return {'message': 'video does not exist'}, 404
