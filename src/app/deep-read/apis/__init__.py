f7e8fcc01d2bf070d04ef669428f3ea9142dab28
ashwin <nashwin2804@gmail.com>
2020-10-22 00:49:45 +0530

    Add APIs for Gallery

--- src/app/deep-read/apis/__init__.py
+++ src/app/deep-read/apis/__init__.py
@@ -2,8 +2,9 @@ from flask_restx import Api
 from .gdrive_api import api as ns1
 from .speech_api import api as ns2
 from .mcq_generator_api import api as ns3
-from .tester_api import api as ns4
-from .summarizer_api import api as ns5
+from .summarizer_api import api as ns4
+from .gallery_api import api as ns5
+from .tester_api import api as ns6
 
 api = Api(
     version="0.1",
@@ -16,4 +17,5 @@ api.add_namespace(ns2)
 api.add_namespace(ns3)
 api.add_namespace(ns4)
 api.add_namespace(ns5)
+api.add_namespace(ns6)
 
