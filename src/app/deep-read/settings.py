2b93f73b52e1569cfd1ad942101db00a2f5f809c
Aquil Hassan <acquil98@gmail.com>
2020-10-23 17:02:30 +0000

    Added sentence segmentation api to speech_api.py

--- src/app/deep-read/settings.py
+++ src/app/deep-read/settings.py
@@ -1,5 +1,5 @@
 """
-Settings for the polls application.
+Settings for the application.
 
 You can set values of REPOSITORY_NAME and REPOSITORY_SETTINGS in
 environment variables, or set the default values in code here.
@@ -9,12 +9,14 @@ from os import environ
 
 # default storage
 REPOSITORY_NAME = environ.get('REPOSITORY_NAME', 'memory')
+USE_SENTENCE_SEGMENTER = False
+SEGMENTER_KEY = environ.get('DEEPSEGMENT_API_KEY', '')
 
 if REPOSITORY_NAME == 'mongodb':
     REPOSITORY_SETTINGS = {
         'MONGODB_HOST': environ.get('MONGODB_HOST', None),
         'MONGODB_DATABASE': environ.get('MONGODB_DATABASE', 'test'),
-        'MONGODB_COLLECTION': environ.get('MONGODV_COLLECTION', 'videos')
+        'MONGODB_COLLECTION': environ.get('MONGODB_COLLECTION', 'videos')
     }
 elif REPOSITORY_NAME == 'memory':
     REPOSITORY_SETTINGS = {}
