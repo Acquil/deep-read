fea9376999a1c2045bc165a3811d317c3313128c
Aquil Hassan <acquil98@gmail.com>
2020-10-23 17:04:31 +0000

    Added in-memory repo global variable and refactored code.
    Former-commit-id: 1171d4c35da11b4954605cdcf286b70997c89fc0

--- src/app/deep-read/db/factory.py
+++ src/app/deep-read/db/factory.py
@@ -1,14 +1,26 @@
 """
 Factory for the different types of db/repositories.
 """
+
+# Only if you are storing objects in the memory
+_local_connection_obj = None
+
+
 def create_repository(name, settings):
     """Creates a repository from its name and settings. The settings
     is a dictionary where the keys are different for every type of repository.
     See each repository for details on the required settings."""
     if name == 'mongodb':
         from .mongodb import Repository
+
     elif name == 'memory':
         from .memory import Repository
+
+        global _local_connection_obj
+        if _local_connection_obj is None:
+            _local_connection_obj = Repository(settings)
+        return _local_connection_obj
+
     else:
         raise ValueError('Unknown repository.')
 
