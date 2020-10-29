176b09e29796ede34a36bd43b07857b4116244f2
Acquil <acquil98@gmail.com>
2020-10-29 15:41:40 +0530

    Refactored settings.py
    Former-commit-id: 644e9e111a720fae561d3f63efd08f22abe629a9

--- src/app/deep-read/settings.py
+++ src/app/deep-read/settings.py
@@ -9,19 +9,9 @@ from os import environ
 
 # default storage
 REPOSITORY_NAME = environ.get('REPOSITORY_NAME', 'memory')
+# deep-segment api for splitting into sentences
 USE_SENTENCE_SEGMENTER = False
 SEGMENTER_KEY = environ.get('DEEPSEGMENT_API_KEY', '')
-
-if REPOSITORY_NAME == 'mongodb':
-    REPOSITORY_SETTINGS = {
-        'MONGODB_HOST': environ.get('MONGODB_HOST', None),
-        'MONGODB_DATABASE': environ.get('MONGODB_DATABASE', 'test'),
-        'MONGODB_COLLECTION': environ.get('MONGODB_COLLECTION', 'videos')
-    }
-elif REPOSITORY_NAME == 'memory':
-    REPOSITORY_SETTINGS = {}
-else:
-    raise ValueError('Unknown repository.')
 
 # Number of processes to spawn for transcription
 TRANSCRIPTION_WORKERS = 4
@@ -30,3 +20,15 @@ TRANSCRIPTION_WORKERS = 4
 AZURE_CONNECT_STR = environ.get('BLOB_CONNECTION_STR')
 AZURE_CONTAINER_NAME = 'images'
 AZURE_URI_PREFIX = 'https://deepread.blob.core.windows.net/images/'
+
+# Choosing DB
+if REPOSITORY_NAME == 'mongodb':
+    REPOSITORY_SETTINGS = {
+        'MONGODB_HOST': environ.get('MONGODB_HOST', None),
+        'MONGODB_DATABASE': environ.get('MONGODB_DATABASE', 'deep-read'),
+        'MONGODB_COLLECTION': environ.get('MONGODB_COLLECTION', 'videos')
+    }
+elif REPOSITORY_NAME == 'memory':
+    REPOSITORY_SETTINGS = {}
+else:
+    raise ValueError('Unknown repository.')
