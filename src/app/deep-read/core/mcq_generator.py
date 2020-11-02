b52dd6e26eab3251d93378d9c22c361160483d2f
ashwin <nashwin2804@gmail.com>
2020-11-02 10:32:17 +0530

    Improve quality of keyhrases generated in MCQ_GENERATOR
    filter keyphrases belonging to more than one sentence
    filter similar key phrases

--- src/app/deep-read/core/mcq_generator.py
+++ src/app/deep-read/core/mcq_generator.py
@@ -4,6 +4,7 @@ nltk.download('stopwords')
 import pke
 import os
 import uuid
+import re
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from gensim.models import KeyedVectors
@@ -27,33 +28,61 @@ class McqGenerator(object):
 
     def __init__(self, text):
         self.text = text
-        self.filename = str(uuid.uuid4()) + '.txt'
+        self.filename = os.path.join("core/temp/", str(uuid.uuid4()) + '.txt')
+        self.sentences = self.text.split('.')
         self.options = None
         self.questions = None
-        f = open(self.filename, "w")
-        f.write(self.text)
-        f.close()
+        
 
-    # Keyphrases are generated using single rank method
-    def generate_keyphrases(self, n=30):
-        pos = {'NOUN'}
-        extractor = pke.unsupervised.SingleRank()
+    def remove_punctuations(self):
+        return re.sub(r'[^\w\s]', '', self.text) 
+
+    # Keyphrases are generated using text rank method
+    def generate_keyphrases(self, n=30):                
+        with open(self.filename, "w") as f:
+            f.write(self.remove_punctuations())
+
+        pos = {'NOUN', 'ADJ'}
+        extractor = pke.unsupervised.TextRank()
         extractor.load_document(input=self.filename, language='en')
-        extractor.candidate_selection(pos=pos)
-        extractor.candidate_weighting(window=2, pos=pos)
+        extractor.candidate_weighting(window=2,
+                                  pos=pos,
+                                  top_percent=0.75)
 
         keyphrases_and_scores = extractor.get_n_best(n=n)
         self.keyphrases = [keyphrase_and_score[0] for keyphrase_and_score in keyphrases_and_scores]
         return self.keyphrases
 
-    # filter keyphrases that has more than 2 words
-    def filter_keyphrases(self):
+    def filter_keyphrases(self): 
+        #filter keyphrases belonging to more than one sentence(part of a keyphrase belongs to one sentence and another part belongs to another sentence)
+        filtered_keyphrase_indices = []
+        for sentence in self.sentences:
+            for keyphrase_index in range(len(self.keyphrases)):
+                if sentence.find(self.keyphrases[keyphrase_index]) != -1:
+                    filtered_keyphrase_indices.append(keyphrase_index)
+
+        self.keyphrases = [self.keyphrases[i] for i in filtered_keyphrase_indices]
+
+        # filter keyphrases that has more than 2 words
         keyphrase_indices_to_be_removed = []
         for keyphrase_index in range(len(self.keyphrases)):
             if len(self.keyphrases[keyphrase_index].split(' ')) > 2:
                 keyphrase_indices_to_be_removed.append(keyphrase_index)
         for i in reversed(keyphrase_indices_to_be_removed):
             del self.keyphrases[i]
+
+        #filter similar key phrases
+        keyphrase_indices_to_be_removed = []
+        for keyphrase_index in range(len(self.keyphrases)-1):
+            if not keyphrase_index in keyphrase_indices_to_be_removed:
+                for keyphrase_index_2 in range(keyphrase_index+1, len(self.keyphrases)):
+                    if fuzz.ratio(self.keyphrases[keyphrase_index],self.keyphrases[keyphrase_index_2]) > 80:
+                        keyphrase_indices_to_be_removed.append(keyphrase_index_2)
+        for i in reversed(keyphrase_indices_to_be_removed):
+            try:
+                del self.keyphrases[i]
+            except:
+                print(i, len(self.keyphrases))
 
     def generate_distractors(self, answer, count):  # distractors are incorrect options
         # Extracting closest words for the answer.
@@ -94,8 +123,7 @@ class McqGenerator(object):
 
     def generate_questions(self):
         self.questions = [0] * len(self.options)
-        sentences = self.text.split('.')
-        for sentence in sentences:
+        for sentence in self.sentences:
             for i in range(len(self.keyphrases)):
                 if self.questions[i] == 0 and self.keyphrases[i].lower() in sentence.lower():
                     self.questions[i] = sentence.lower().replace(self.keyphrases[i].lower(), "___________", 1)
