14e2f82fe13491ace77f1b074d5066f26d0f9da5
ashwin <nashwin2804@gmail.com>
2020-11-10 20:59:47 +0530

    Fix bugs related to venv path in setup.sh

--- src/app/deep-read/setup.sh
+++ src/app/deep-read/setup.sh
@@ -63,8 +63,8 @@ echo -e "---------------------------------------------------------------------"
 if [ ! -d "$VENV_DIRECTORY" ]; then
 # Control will enter here if $DIRECTORY doesn't exist.
     echo "Setting up venv"
-    python3 -m venv ./venv
-    source ./venv/bin/activate
+    python3 -m venv ./.venv
+    source ./.venv/bin/activate
     pip install -r ./requirements.txt
 fi
 
