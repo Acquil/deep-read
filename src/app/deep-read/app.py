bca39185c1161268264f170cf069d28391b6f9d1
Aquil Hassan <acquil98@gmail.com>
2020-11-17 03:07:22 +0530

    Updates: bug fixes

--- src/app/deep-read/app.py
+++ src/app/deep-read/app.py
@@ -11,4 +11,4 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api.init_app(app)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0',port=5000)
